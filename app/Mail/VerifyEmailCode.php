0878e9949486c10ae88b853cf003be3cfb3d6663
Oumaima Maznine <oumaimaznine1@gmail.com>
2025-05-17 22:50:02 +0100

    cmd

---
+++ app/Mail/VerifyEmailCode.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\User;
+
+class VerifyEmailCode extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user; // Pour accéder au code dans le template
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function build()
+    {
+        return $this->subject('Code de vérification')
+                    ->view('emails.verify_code');
+    }
+}
--- database/migrations/2025_05_17_183929_add_verification_code_to_users_table.php
+++ database/migrations/2025_05_17_183929_add_verification_code_to_users_table.php
@@ -1,6 +1,4 @@
 <?php
-// database/migrations/xxxx_xx_xx_add_verification_code_to_users_table.php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
